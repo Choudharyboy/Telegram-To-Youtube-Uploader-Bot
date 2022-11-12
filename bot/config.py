59c72b0a7fd7d48ba33a13b47bb6c9e564c80b2c
Choudharyboy <115158030+Choudharyboy@users.noreply.github.com>
2022-11-12 09:02:36 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,19 +2,19 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "5683916988:AAEmdqUZCQYRbkwUsASSAnpNdamBahWy6z0"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "anuchoudharyyybot"
 
-    API_ID = ""
+    API_ID = "18451131"
 
-    API_HASH = ""
+    API_HASH = "aff8e25d4fd3553ee85ced886f90c655"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "http://81760464266-i61pia58j7505rk2bhd2230s1qj92h8v.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [5651503575]
 
     VIDEO_DESCRIPTION = ""
 
