e42dceec6b54aeb105f42847c0a745f08cde52ce
Choudharyboy <115158030+Choudharyboy@users.noreply.github.com>
2022-11-14 12:01:25 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,17 +2,17 @@ import os
 
 class Config:
 
-    BOT_TOKEN = "5683916988:AAEmdqUZCQYRbkwUsASSAnpNdamBahWy6z0"
+    BOT_TOKEN = "5614267063:AAGSxKlFku3kNfv53zFb5-ZNFwLDBwt3YBc"
 
-    SESSION_NAME = "anuchoudharyyybot"
+    SESSION_NAME = "Youtubuploderbot"
 
     API_ID = "18451131"
 
     API_HASH = "aff8e25d4fd3553ee85ced886f90c655"
 
-    CLIENT_ID = "http://81760464266-i61pia58j7505rk2bhd2230s1qj92h8v.apps.googleusercontent.com"
+    CLIENT_ID = "http://81760464266-v6298vc49211ii6fck9lkpp8gmfslk2k.apps.googleusercontent.com"
 
-    CLIENT_SECRET = "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"
+    CLIENT_SECRET = "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"
 
     AUTH_USERS = [5651503575]
 
